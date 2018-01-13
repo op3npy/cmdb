f4f5176cbb13c57169cca3389f3976c98183d1d3
Unknown <shadon.py@protonmail.com>
2018-01-13 16:56:48 +0800

    fix:环境变量冲突问题

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -1,9 +1,9 @@
 import os
 
-module = os.environ.get('DJANGO_SETTINGS_MODULE', None) == 'production'
+module = os.environ.get('DJANGO_RUNNING_ENVIRON', None)
 if module is None or module == 'dev':
-    from cmdb.settings_dir.production_settings import *
-elif module == 'production':
     from cmdb.settings_dir.dev_settings import *
+elif module == 'production':
+    from cmdb.settings_dir.production_settings import *
 elif module == 'test':
     from cmdb.settings_dir.test_settings import *
---
+++ record.md
@@ -0,0 +1,7 @@
+* 命令记录
+
+
+django 多数据库创建
+python .\manage.py makemigrations db_name
+python .\manage.py migrate --database resource
+
