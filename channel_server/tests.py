99b543a14e6ebd28e2f89c71d23ae59840951173
Unknown <shadon.py@protonmail.com>
2018-02-06 19:06:19 +0800

    test:channels测试

--- channel_server/tests.py
+++ channel_server/tests.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by zhou on 2018/2/5
-from channels.test import ChannelTestCase, Client
+import os
+
+from channels import route_class
+from channels.test import ChannelTestCase, Client, apply_routes, WSClient
+
+from channel_server.consumer import ServerConsumer
 
 
 class MyTests(ChannelTestCase):
+
     def test_a_thing(self):
         # Inject a message onto the channel to use in a consumer
-        client = Client()
-        client.send_and_consume('websocket.connect', {'value': 'my_value'})
-        # connect/disconnect is successful or not
-        self.assertEqual(client.receive(), {'value': 'my_value'})
+        with apply_routes(route_class(ServerConsumer, path='/inner')):
+            client = WSClient()
+            client.send_and_consume('websocket.connect', path='/inner', content={'value': 'my_value'})
+            # connect/disconnect is successful or not
+            self.assertEqual(client.receive(), {'value': 'my_value'})
